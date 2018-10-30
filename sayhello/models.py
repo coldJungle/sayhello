cdb5b2fdb990d9ca8e1ecca7f90e478940534d8d
elgnuj <760201504@qq.com>
2018-10-30 13:09:08 +0800

    almost done

--- sayhello/models.py
+++ sayhello/models.py
@@ -7,4 +7,6 @@ class Message(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(20))
     body = db.Column(db.String(200))
-    timestamp = db.Column(db.Datetime, default=datetime.utcnow, index=True)
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow, index=True)
+
+    
